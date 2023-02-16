39e319319e46d617dba27f9b879f93a66fc6eeed
Chris Teyerl <chris.teyerl@cosamey.com>
2023-02-16 22:04:02 +0100

    Install Livewire 2 📦

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -7,9 +7,12 @@
         <title>{{ config('app.name') }}</title>
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        @livewireStyles
     </head>
 
     <body>
 
+        @livewireScripts
     </body>
 </html>

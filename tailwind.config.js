85c7adf5a01e7fe4d1303a98878a98802974b0ec
Chris Teyerl <chris.teyerl@cosamey.com>
2023-02-16 22:00:51 +0100

    Install Tailwind CSS 3 📦

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './resources/**/*.{blade.php,js,vue}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
+};

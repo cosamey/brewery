b3453b4349bd89d1c11891e0995f8d1d7bb697f6
Chris Teyerl <chris.teyerl@cosamey.com>
2023-02-24 08:59:50 +0100

    Fix missing enum imports

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Enums\{DeliveryMethod, PaymentMethod};
+use App\Enums\{DeliveryMethod, OrderStatus, PaymentMethod};
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\{Model, Relations};

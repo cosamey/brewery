026a153569960041233769351e8c1ff36a91594e
Chris Teyerl <chris.teyerl@cosamey.com>
2023-02-24 08:55:47 +0100

    Add function to format prices in helpers

--- app/helpers.php
+++ app/helpers.php
@@ -6,3 +6,12 @@ if (! function_exists('cart')) {
         return app('cart');
     }
 }
+
+if (! function_exists('price_format')) {
+    function price_format(float $price, string $currency = 'EUR'): string
+    {
+        $formatter = new NumberFormatter(app()->getLocale(), NumberFormatter::CURRENCY);
+
+        return $formatter->formatCurrency($price, $currency);
+    }
+}

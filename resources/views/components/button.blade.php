eb2d16d77753938fa6ebbb754fd12aad1cac1d78
Chris Teyerl <chris.teyerl@cosamey.com>
2023-02-25 11:27:40 +0100

    Enhance basic button

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -9,9 +9,11 @@
 
 <{{ $element }}
     {{ $attributes->class([
-        'relative inline-flex items-center justify-center rounded-2xl px-10 py-5 text-center text-xl font-bold uppercase active:brightness-90 sm:px-14 sm:text-2xl',
+        'relative inline-flex items-center justify-center rounded-2xl px-10 py-5 text-center text-xl font-bold uppercase shadow-[0_0_10px_rgba(0,0,0,0.25)] active:brightness-90 sm:px-14 sm:text-2xl',
         'bg-blue text-black' => $color === 'blue',
         'bg-rose text-black' => $color === 'rose',
+        'bg-white text-black' => $color === 'white',
+        'bg-black text-white' => $color === 'black',
     ]) }}
 >
     {{ $slot }}

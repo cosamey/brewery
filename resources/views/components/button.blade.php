ce89d65050c54795b1aae64c36cb1136d81c04ed
Chris Teyerl <chris.teyerl@cosamey.com>
2023-02-25 20:59:21 +0100

    Enhance basic and icon button

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -9,11 +9,11 @@
 
 <{{ $element }}
     {{ $attributes->class([
-        'relative inline-flex items-center justify-center rounded-2xl px-10 py-5 text-center text-xl font-bold uppercase shadow-[0_0_10px_rgba(0,0,0,0.25)] active:brightness-90 sm:px-14 sm:text-2xl',
-        'bg-blue text-black' => $color === 'blue',
-        'bg-rose text-black' => $color === 'rose',
-        'bg-white text-black' => $color === 'white',
-        'bg-black text-white' => $color === 'black',
+        'relative inline-flex items-center justify-center rounded-2xl px-10 py-5 text-center text-xl font-bold uppercase shadow-[0_0_5px_rgba(0,0,0,0.25)] active:brightness-90 sm:px-14 sm:text-2xl',
+        'shadow-black bg-black text-white shadow-black' => $color === 'black',
+        'shadow-blue bg-blue text-black shadow-blue' => $color === 'blue',
+        'shadow-rose bg-rose text-black shadow-rose' => $color === 'rose',
+        'shadow-white bg-white text-black shadow-white' => $color === 'white',
     ]) }}
 >
     {{ $slot }}
--- resources/views/components/icon-button.blade.php
+++ resources/views/components/icon-button.blade.php
@@ -9,9 +9,11 @@
 
 <{{ $element }}
     {{ $attributes->class([
-        'relative inline-flex items-center justify-center rounded-full p-4',
-        'bg-blue text-black' => $color === 'blue',
-        'bg-rose text-black' => $color === 'rose',
+        'relative inline-flex items-center justify-center rounded-full p-4 shadow-[0_0_5px_rgba(0,0,0,0.25)] active:brightness-90',
+        'bg-black text-black shadow-black' => $color === 'black',
+        'bg-blue text-black shadow-blue' => $color === 'blue',
+        'bg-rose text-black shadow-rose' => $color === 'rose',
+        'bg-white text-black shadow-white' => $color === 'white',
     ]) }}
 >
     {{ $slot }}

8df12ecad83667a728000f8b18ed5f1afcbc23e4
Chris Teyerl <chris.teyerl@cosamey.com>
2023-02-27 09:00:31 +0100

    Redirect to home page when cart is empty

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
 
 class CheckoutController extends Controller
 {
-    public function __invoke(): View
+    public function __invoke(): View|RedirectResponse
     {
+        if (cart()->count() === 0) {
+            return to_route('pages.home');
+        }
+
         return view('pages.checkout', [
             'items' => cart()->items(),
             'tax' => cart()->tax(),

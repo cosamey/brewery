8e0f5246f51a09444ddb2766a84287e401e31e14
Chris Teyerl <chris.teyerl@cosamey.com>
2023-03-05 19:48:11 +0100

    Enhance basic button

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -9,7 +9,7 @@
 
 <{{ $element }}
     {{ $attributes->class([
-        'relative inline-flex items-center justify-center rounded-2xl px-10 py-5 text-center text-xl font-bold uppercase shadow-[0_0_5px_rgba(0,0,0,0.25)] active:brightness-90 sm:px-14 sm:text-2xl',
+        'relative inline-flex items-center justify-center rounded-2xl px-10 py-5 text-center text-xl font-bold uppercase shadow-[0_0_5px_rgba(0,0,0,0.25)] transition-transform hover:scale-105 active:brightness-90 sm:px-14 sm:text-2xl',
         'shadow-black bg-black text-white shadow-black' => $color === 'black',
         'shadow-blue bg-blue text-black shadow-blue' => $color === 'blue',
         'shadow-rose bg-rose text-black shadow-rose' => $color === 'rose',
